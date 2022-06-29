6260cf705f9228b6e17d58b90b7188c5de2d80b5
Claudio Barsante <clbmribas@gmail.com>
2022-06-29 16:52:49 -0300

    feat: add <Footer/>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const index = () => {
-	return <div>index</div>;
+  return (
+    <div className="footer-container">
+      <p>2022 Phanox Headphones All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default index;
