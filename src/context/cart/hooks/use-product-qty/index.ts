d5cc5fc6fcd67fd6cf0f47732898dd7a5f27c820
Claudio Barsante <clbmribas@gmail.com>
2022-07-12 13:31:44 -0300

    test: add test to custom hook  useProductQty

--- src/context/cart/hooks/use-product-qty/index.ts
+++ src/context/cart/hooks/use-product-qty/index.ts
@@ -1,19 +1,19 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 export default function useProductQty() {
   const [qty, setQty] = useState<number>(1);
 
-  const increaseQty = () => {
+  const increaseQty = useCallback(() => {
     setQty((prevQty) => prevQty + 1);
-  };
+  }, []);
 
-  const decreaseQty = () => {
+  const decreaseQty = useCallback(() => {
     setQty((prevQty) => {
       if (prevQty - 1 < 1) return 0;
 
       return prevQty - 1;
     });
-  };
+  }, []);
 
   return { qty, increaseQty, decreaseQty };
 }
---
+++ src/context/cart/hooks/use-product-qty/test.ts
@@ -0,0 +1,30 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useProductQty from './';
+
+describe('useProductQty', () => {
+  it('should increase product quantity', () => {
+    const { result } = renderHook(() => useProductQty());
+
+    act(() => {
+      result.current.increaseQty();
+    });
+
+    expect(result.current.qty).toBe(2);
+  });
+
+  it('should decrease product quantity', () => {
+    const { result } = renderHook(() => useProductQty());
+
+    act(() => {
+      result.current.increaseQty();
+      result.current.decreaseQty();
+    });
+
+    expect(result.current.qty).toBe(1);
+
+    act(() => {
+      result.current.decreaseQty();
+    });
+    expect(result.current.qty).toBe(0);
+  });
+});
